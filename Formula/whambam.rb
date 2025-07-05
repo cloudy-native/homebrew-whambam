2696fdc51e6ea62e7cfb9f49dc9074a3ec7073c2
Stephen Harrison <stephen@harrison.org>
2025-07-05 20:04:09 +0700

    v0.1.5

--- Formula/whambam.rb
+++ Formula/whambam.rb
@@ -2,7 +2,7 @@ class Whambam < Formula
     desc "whambam: a simple tool for web performance testing"
     homepage "https://github.com/cloudy-native/whambam.dev"
     url "https://github.com/cloudy-native/whambam.dev/archive/refs/tags/v0.1.5.tar.gz"
-    sha256 "3ab1b2690615cf91f486843e0ec5cadaa168699e5cc8ed03835392eb075af8da"
+    sha256 "274e9a0a149e4654a70712a34a09cab8e09a655200de02bf7cbc080c196d5ffb"
     license "MIT"
     head "https://github.com/cloudy-native/whambam.dev.git", branch: "main"
   
