64efc0466a965044d7cf8198f7f1eb46fae1309d
Stephen Harrison <stephen@harrison.org>
2025-07-06 13:16:56 +0700

    Bump v0.1.7

--- Formula/whambam.rb
+++ Formula/whambam.rb
@@ -1,18 +1,24 @@
 class Whambam < Formula
-    desc "whambam: a simple tool for web performance testing"
-    homepage "https://github.com/cloudy-native/whambam.dev"
-    url "https://github.com/cloudy-native/whambam.dev/archive/refs/tags/v0.1.5.tar.gz"
-    sha256 "274e9a0a149e4654a70712a34a09cab8e09a655200de02bf7cbc080c196d5ffb"
-    license "MIT"
-    head "https://github.com/cloudy-native/whambam.dev.git", branch: "main"
-  
-    depends_on "rust" => :build
-  
-    def install
-      system "cargo", "install", *std_cargo_args
-    end
-  
-    test do
-      system "#{bin}/whambam", "--version"
+  desc "whambam: a simple CLI tool for web performance testing"
+  homepage "https://github.com/cloudy-native/whambam.dev"
+  license "MIT"
+  head "https://github.com/cloudy-native/whambam.dev.git", branch: "main"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/cloudy-native/whambam.dev/releases/download/v0.1.5/whambam-v0.1.7-aarch64-apple-darwin.tar.gz"
+      sha256 "sha256:26150f085d15a3477e86a6939a34600334548f9808d5d4e8e49a2e25ddee0695"
+    else
+      url "https://github.com/cloudy-native/whambam.dev/releases/download/v0.1.5/whambam-v0.1.7-x86_64-apple-darwin.tar.gz"
+      sha256 "sha256:9f4ebe80ac463380ebd2e76c414da0b7ca48c617db18bff7de591337202257f8"
     end
   end
+
+  def install
+    bin.install "whambam"
+  end
+
+  test do
+    system "#{bin}/whambam", "--version"
+  end
+end
